5adf497504aa53bad22d52d54c826b921f6e4dec
ensp1re <ostapuk1318@gmail.com>
2024-11-29 14:17:37 +0200

    fixed forms.py

--- forms.py
+++ forms.py
@@ -1,3 +1,4 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
